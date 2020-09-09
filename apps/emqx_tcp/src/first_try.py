993538c45d30c74d24c4b22c347cb6fa33aeab84
scarlett-bai <51374127+scarlett-bai@users.noreply.github.com>
2020-09-09 23:38:08 +0800

    just a try (#1)
    just a try

---
+++ apps/emqx_tcp/src/first_try.py
@@ -0,0 +1,11 @@
+import time
+import random
+
+def main():
+    print('hello github')
+    time.sleep(1)
+    print('the number is {}'.format(random.randint(0, 10)))
+
+
+if __name__ == '__main__':
+    main()
